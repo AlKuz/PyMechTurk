87450f096a5e85c6567f56d464529f4910931caa
Alexander Kuznetsov <a.v.kuznetsov31@gmail.com>
2020-05-30 19:18:53 +0400

    Add XML generator for qualification test

---
+++ pymechturk/qualification/__init__.py
@@ -0,0 +1,3 @@
+from pymechturk.qualification.data_classes import QualificationType
+from pymechturk.qualification.xml_generator import XMLWrapper, Content, Question, Answer, SelectionAnswer,\
+    FreeTextAnswer, AnswerKey, QuestionForm
